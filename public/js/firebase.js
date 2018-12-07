79019f8c5a8460262e85a4febf08c315ebd024bc
Jose Pablo Valle <jpvalle@ucsd.edu>
2018-12-07 03:32:33 -0800

    falling asleep

---
+++ public/js/firebase.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCqA1IGa5n5MR5rHSSUs1w4mw-KXp8NS2s",
+    authDomain: "gotem-memes-243ad.firebaseapp.com",
+    databaseURL: "https://gotem-memes-243ad.firebaseio.com",
+    projectId: "gotem-memes-243ad",
+    storageBucket: "gotem-memes-243ad.appspot.com",
+    messagingSenderId: "865423099288"
+  };
+  firebase.initializeApp(config);
---
+++ public/js/index.js
@@ -0,0 +1,88 @@
+function populate_DOM(db, us)
+{
+  db.collection("users").get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+      console.log(doc.id, " => ", doc.data());
+      const data = doc.data();
+      for(let item in data){
+        console.log(item+": "+data[item]);
+        const itemizer = data[item];
+        for(let thing in itemizer[0]){
+          console.log(thing+": "+itemizer);
+        }
+
+        item.path;
+        const templates = document.querySelector("#templates");
+        let next_grid_item = document.querySelector(".grid").firstChild;
+
+        //go to the end of the list
+        while(next_grid_item.nextElementSibling)
+        {
+          next_grid_item = next_grid_item.nextElementSibling;
+        }
+
+        //pull from the database
+        if(true == false){
+
+          for(let image_db in null){
+            const image_path_gs = db.collection("users").doc(image_db);
+            for(let thing in image_path_gs){
+              console.log(thing+": "+image_path_gs[thing]);
+            }
+
+            // Create a reference with an initial file path and name
+            try{
+
+              // Create a reference from a Google Cloud Storage URI
+              const gsReference = us.ref(image_path_gs.path);
+
+              gsReference.getDownloadURL().then(function(url) {// `url` is the download URL for the user's image
+
+                //get the template
+                const grid_item_template = templates.querySelector('#grid-item-template');
+
+                //strip out the movie-item element from the template
+                const grid_item = document.importNode(grid_item_template.content, true);
+
+                //insert into the <img> element:
+                grid_item.querySelector('.grid-item-image').src = "{doc.data()}";
+
+                //add the item to the list 
+                next_grid_item.nextElementSibling = grid_item;
+                next_grid_item = next_grid_item.nextSibling;
+
+              }).catch(function(error) {
+                // Handle any errors
+                console.error(error.toString());
+                // A full list of error codes is available at
+                // https://firebase.google.com/docs/storage/web/handle-errors
+                switch (error.code) {
+                  case 'storage/object-not-found':
+                    // File doesn't exist
+                    break;
+
+                  case 'storage/unauthorized':
+                    // User doesn't have permission to access the object
+                    break;
+
+                  case 'storage/canceled':
+                    // User canceled the upload
+                    break;
+
+                  case 'storage/unknown':
+                    // Unknown error occurred, inspect the server response
+                    break;
+                }
+              });
+
+            }
+            catch(err){
+              console.error(err);
+            }
+          }
+          alert('You need to login to view your library');
+        }
+      }
+    });
+  });
+}
